ec9dea805c1ff70495bc9d2984ffa4c8a00a1e5d
Prayansh <Pradyvisen@gmail.com>
2024-12-09 10:31:01 +0530

    added code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { port } from './config/serverConfig.js';
+import cors from 'cors';
+import {  PORT } from './config/server.Config.js';
 
 
 const app = express();
 
-app.listen(port, () => {
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${port}`);
-    }); 
+});
+
+app.get('/ping', (req, res) => {
+    return res.json({ message: 'pong' });
+});
+
